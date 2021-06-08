dcb29df879a1689d796bc8e840adc37e4dd54fb0
Jitendra <jeetscmaker@gmail.com>
2021-06-08 23:47:32 +0530

    contest: Codeforces Round #109 (Div. 2), problem: (A) I_love_\%username\%

--- C++/a.cpp
+++ C++/a.cpp
@@ -5,9 +5,26 @@
  * contest: Codeforces Round #322 (Div. 2), problem: (A) Vasya the Hipster
  */
 	int main() {
-		 int a,b;
-		 cin>>a>>b;
-		 cout<<(a>b?b:a) << " " << (a>b?(a-b)/2:(b-a)/2);
+		 int n;
+		 cin>>n;
+		 int a[n];
+		 int count = 0;
+		 for(int i = 0; i < n; i++){
+			 cin>>a[i];
+		 }
+		 int min = a[0], max = a[0];
+		 for(int i = 1; i < n; i++){
+			 if(a[i] < min){
+				 count++;
+				 min = a[i];
+			 }
+			else if(a[i] > max){
+				 count++;
+				 max = a[i];
+			 }
+			 else {}
+		 }
+		 cout<<count;
 		 return 0;	
 	}
 		
