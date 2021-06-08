c4df73d04db96cf0430beedace1bb978781e2938
Jitendra <jeetscmaker@gmail.com>
2021-06-08 21:33:44 +0530

    contest: Codeforces Round #322 (Div. 2), problem: (A) Vasya the Hipster,

--- C++/a.cpp
+++ C++/a.cpp
@@ -2,29 +2,13 @@
 
 	using namespace std;
 /**
- * contest: Codeforces Round #164 (Div. 2), problem: (A) Games
+ * contest: Codeforces Round #322 (Div. 2), problem: (A) Vasya the Hipster
  */
 	int main() {
-		 int n;
-		 cin>>n;
-		 int h[n], g[n];
-		 for(int i=0; i<n; i++){
-			 cin>>h[i]>>g[i];
-		 }
-		 int count = 0;
-		 for(int i=0; i<n; i++){
-			 for(int j=0; j<n; j++){
-				 if(i == j){
-					 continue;
-				 }
-				 else {
-					 if(h[i] == g[j])
-						 count++;
-				 }
-			 }
-		 }
-		 cout<<count;
-		return 0;
+		 int a,b;
+		 cin>>a>>b;
+		 cout<<(a>b?b:a) << " " << (a>b?(a-b)/2:(b-a)/2);
+		 return 0;	
 	}
 		
 	
