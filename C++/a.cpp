924e638b7913469b7d3a109158964f9b05207e11
Jitendra <jeetscmaker@gmail.com>
2021-06-10 01:08:58 +0530

    contest: Codeforces Round #661 (Div. 3), problem: (B) Gifts Fixing

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,39 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-/*
-* contest: Codeforces Round #642 (Div. 3), problem: (B) Two Arrays And Swaps
-*/
-int main(){
-	int t;
-	cin>>t;
-	int n, k;
-	
-	while(t--){
-		cin>>n>>k;
-		int a[n], b[n];
-		for(int i = 0; i<n; i++){
-			cin>>a[i];
-		}
-		for(int i = 0; i<n; i++){
-			cin>>b[i];
-		}
-		sort(a, a+n);
-		sort(b, b+n);
-		int r = 0, s = n-1;
-		for(int i=0; i<k; i++){
-			if(a[r]<b[s]){
-				swap(a[r], b[s]);
-				r++;
-				s--;
-			}
-		}
-		int sum = 0;
-		for(auto element : a)
-			sum += element;
-		cout<<sum<<endl;
-	}
-	return 0;
-}
+const int MIN = 1e9;
 
-		
-	
+int main() {
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        int a[n], b[n];
+        int am = MIN, bm = MIN;
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+            if(am > a[i])
+				am = a[i];
+        }
+         for(int i=0; i<n; i++){
+            cin>>b[i];
+            if(bm > b[i])
+				bm = b[i];
+        }
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum = sum + (a[i]-am) + (b[i]-bm);
+        }
+        cout<<sum<<endl;
+        
+    }
+    return 0;
+}
