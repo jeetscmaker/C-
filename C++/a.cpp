e4dd315ceacb08233d3f57bace9ec1b49dc9b2ef
Jitendra <jeetscmaker@gmail.com>
2021-06-09 17:58:00 +0530

    contest: Codeforces Round #642 (Div. 3), problem: (B) Two Arrays And Swaps

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,29 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 /*
-* contest: Codeforces Round #101 (Div. 2), problem: (A) Amusing Joke
+* contest: Codeforces Round #642 (Div. 3), problem: (B) Two Arrays And Swaps
 */
 int main(){
-	string a,b,c;
-	cin>>a>>b>>c;
-	string d = a+b;
-	if(d.length() != c.length()){
-		cout<<"NO";
-		return 0;
-	}
-	sort(d.begin(), d.end());
-	sort(c.begin(), c.end());
-	bool is_equal = true;
-	for(int i = 0; i<d.length(); i++){
-		if(d[i] != c[i]){
-			is_equal = false;
-			break;
+	int t;
+	cin>>t;
+	int n, k;
+	
+	while(t--){
+		cin>>n>>k;
+		int a[n], b[n];
+		for(int i = 0; i<n; i++){
+			cin>>a[i];
 		}
+		for(int i = 0; i<n; i++){
+			cin>>b[i];
+		}
+		sort(a, a+n);
+		sort(b, b+n);
+		int r = 0, s = n-1;
+		for(int i=0; i<k; i++){
+			if(a[r]<b[s]){
+				swap(a[r], b[s]);
+				r++;
+				s--;
+			}
+		}
+		int sum = 0;
+		for(auto element : a)
+			sum += element;
+		cout<<sum<<endl;
 	}
-	if(is_equal)
-		cout<<"YES";
-	else
-		cout<<"NO";
 	return 0;
 }
 
