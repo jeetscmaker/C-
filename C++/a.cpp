31d645d1421e4602eff81409500d05b6dd5f0192
Jitendra <jeetscmaker@gmail.com>
2021-06-14 00:47:21 +0530

    #include <bits/stdc++.h>

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,33 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 /**
- * contest: Codeforces Round #365 (Div. 2), problem: (A) Mishka and Game
+ * contest: Codeforces Round #644 (Div. 3), problem: (B) Honest Coach
  * */
 int main() {
-	int n;
-	cin>>n;
-	int m[n], c[n];
-	int mishka_wins = 0, chris_wins = 0;
-	for (int i = 0; i < n; ++i)
-	{
-		cin>>m[i]>>c[i];
-		if (m[i] > c[i])
+	int t;
+	cin>>t;
+	while(t--){
+		short n;
+		cin>>n;
+		int a[n];
+		for (int i = 0; i < n; ++i)
 		{
-			mishka_wins++;
+			cin>>a[i];
 		}
-		else if (c[i] > m[i])
+		sort(a, a+n);
+		int min = 1001;
+		for (int i = 1; i <= n; ++i)
 		{
-			chris_wins++;
+			if((a[i] - a[i-1]) < min){
+				min = a[i] - a[i-1];
+			}
 		}
-		else{
-			// do nothing, it's a draw.
-		}
+		cout<<min<<endl;
 	}
-	if (mishka_wins > chris_wins)
-			cout<<"Mishka";
-		else if(chris_wins > mishka_wins)
-			cout<<"Chris";
-		else
-			cout<<"Friendship is magic!^^";
 	return 0;
 }
