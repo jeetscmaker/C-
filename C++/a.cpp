e9b89174c52f19d04de9d9099947f1e2eb3015bf
Jitendra <jeetscmaker@gmail.com>
2021-06-15 09:43:41 +0530

    maximum sum of a subarray which is contiguous.

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,28 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
 /**
- * contest: Codeforces Round #644 (Div. 3), problem: (B) Honest Coach
+ * Find the maximum sum of a contiguous subarray of size k in a given array
+ * Input  : arr[] = {100, 200, 300, 400}, k = 2, Output : 700 {300,400}
+ * Input  : arr[] = {1, 4, 2, 10, 23, 3, 1, 0, 20}, k = 4, Output : 39 {4, 2, 10, 23}
  * */
-int main() {
-	int t;
-	cin>>t;
-	while(t--){
-		int n;
-		cin>>n;
-		int a[n];
-		for (int i = 0; i < n; ++i)
-		{
-			cin>>a[i];
-		}
-		sort(a, a+n);
-		int min = 100000;
-		for (int i = 1; i < n; ++i)
-		{
-			if((a[i] - a[i-1]) < min){
-				min = a[i] - a[i-1];
-			}
-		}
-		cout<<min<<endl;
+
+int main(){
+	int n, k;
+	cin>>n>>k;
+	int a[n];
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>a[i];
 	}
+	int sum = 0, max_sum = 0;
+	// sum of first k elements
+	for (int i = 0; i < k; ++i)
+	{
+		sum += a[i];
+	}
+	int start = 0;
+	for (int i = k; i < n; ++i)
+	{
+		if(sum > max_sum){
+			max_sum = sum;
+		}
+		sum  = sum - a[start] + a[i];
+		start++;
+	}
+	if(sum > max_sum){
+			max_sum = sum;
+	}
+	cout<<"max sum is: "<<max_sum;
 	return 0;
 }
