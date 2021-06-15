6238cde44c63440531f1edbe631b1945e32780ae
Jitendra <jeetscmaker@gmail.com>
2021-06-15 23:58:16 +0530

    contest: Codeforces Round #654 (Div. 2), problem: (A) Magical Sticks

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,39 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+// contest: Codeforces Round #654 (Div. 2), problem: (A) Magical Sticks
+using ll = long long;
 
-/**
- * Find the maximum sum of a contiguous subarray of size k in a given array
- * Input  : arr[] = {100, 200, 300, 400}, k = 2, Output : 700 {300,400}
- * Input  : arr[] = {1, 4, 2, 10, 23, 3, 1, 0, 20}, k = 4, Output : 39 {4, 2, 10, 23}
- * */
-
-int main(){
-	int n, k;
-	cin>>n>>k;
-	int a[n];
-
-	for (int i = 0; i < n; ++i)
-	{
-		cin>>a[i];
-	}
-	int sum = 0, max_sum = 0;
-	// sum of first k elements
-	for (int i = 0; i < k; ++i)
-	{
-		sum += a[i];
-	}
-	int start = 0;
-	for (int i = k; i < n; ++i)
-	{
-		if(sum > max_sum){
-			max_sum = sum;
-		}
-		sum  = sum - a[start] + a[i];
-		start++;
-	}
-	if(sum > max_sum){
-			max_sum = sum;
-	}
-	cout<<"max sum is: "<<max_sum;
-	return 0;
+int main (){
+  int t;
+  cin >> t;
+  while(t--){
+    double n;
+    cin >> n;
+    long res = ceil(n/2);
+    cout << res << endl;
+  }
+  return 0;
 }
