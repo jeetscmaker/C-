bcf0398f08bbd20d8c0ac300e8d0680f6743500e
Jitendra <jeetscmaker@gmail.com>
2021-06-17 23:10:54 +0530

    contest: Codeforces Round #289 (Div. 2, ACM ICPC Rules), problem: (A) Maximum in Table

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,31 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-// contest: Codeforces Round #624 (Div. 3), problem: (A) Add Odd or Subtract Even
-// choose any positive odd integer x and replace a with a+x.
-// choose any positive even integer y and replace a with a-y.
-// Obtain b from a in minimum number of such operations.
+// contest: Codeforces Round #289 (Div. 2, ACM ICPC Rules), problem: (A) Maximum in Table
 bool is_even(long n) {
     return (n%2 == 0);
 }
 int main() {
-    int t;
-    cin >> t;
-    while(t--) {
-        long a, b;
-        cin >> a >> b;
-        if(a > b){
-            if(is_even(a - b))
-                cout << 1 << endl;
-            else 
-                cout << 2 << endl;
-        } else if(b > a) {
-            if(is_even(b - a))
-                cout << 2 << endl;
-            else 
-                cout << 1 << endl;
-        } else
-            cout << 0 << endl;
-        
+    short n;
+    cin >> n;
+    int a[n][n];
+    for(short i = 0; i<n; i++){
+        a[0][i] = 1;
+        a[i][0] = 1;
     }
+    for(short i = 1; i<n; i++){
+        for(short j = 1; j<n; j++){
+            a[i][j] = a[i-1][j] + a[i][j-1];
+        }
+    }
+    cout << a[n-1][n-1];
 	return 0;
 }
