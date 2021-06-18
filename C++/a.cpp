a9e99d233fe649b9c258cf309c3ed5004819d893
Jitendra <jeetscmaker@gmail.com>
2021-06-19 02:32:27 +0530

    contest: Codeforces Round #693 (Div. 3), problem: (A) Cards for Friends

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,38 +1,24 @@
 #include <bits/stdc++.h>
-
+// contest: Codeforces Round #693 (Div. 3), problem: (A) Cards for Friends
 using namespace std;
-// contest: Codeforces Round #713 (Div. 3), problem: (B) Almost Rectangle
-#define forn(i, n) for (int i = 0; i < int(n); i++)
-
+// 
 int main() {    
     int t;
     cin >> t;
-    forn(tt, t) {
-        int n;
-        cin >> n;
-        vector<string> f(n);
-        vector<pair<int,int>> p;
-        forn(i, n) {
-            cin >> f[i];
-            forn(j, n)
-                if (f[i][j] == '*')
-                    p.push_back({i, j});
-        }
-
-        p.push_back(p[0]);
-        p.push_back(p[1]);
-        if (p[0].first == p[1].first) {
-            p[2].first = (p[2].first + 1) % n;
-            p[3].first = (p[3].first + 1) % n;
-        } else if (p[0].second == p[1].second) {
-            p[2].second = (p[2].second + 1) % n;
-            p[3].second = (p[3].second + 1) % n;
-        } else
-            swap(p[2].first, p[3].first);
-
-        f[p[2].first][p[2].second] = '*';
-        f[p[3].first][p[3].second] = '*';
-        forn(i, n)
-            cout << f[i] << endl;
+    while(t--) {
+        int w, h;
+        long n;
+          cin >> w >> h >> n;
+          int res = 1;
+          while (w % 2 == 0) {
+            w /= 2;
+            res *= 2;
+          }
+          while (h % 2 == 0) {
+            h /= 2;
+            res *= 2;
+          }
+          cout << (res >= n ? "YES\n" : "NO\n");
     }
+    return 0;
 }
