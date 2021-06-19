5c3246d6ceaae35ae065e6f1271503272cde1105
Jitendra <jeetscmaker@gmail.com>
2021-06-19 23:31:23 +0530

    contest: Codeforces Round #368 (Div. 2), problem: (A) Brain's Photos

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
-// contest: Codeforces Round #693 (Div. 3), problem: (A) Cards for Friends
 using namespace std;
-// 
+
+// contest: Codeforces Round #368 (Div. 2), problem: (A) Brain's Photos
+ 
 int main() {    
-    int t;
-    cin >> t;
-    while(t--) {
-        int w, h;
-        long n;
-          cin >> w >> h >> n;
-          int res = 1;
-          while (w % 2 == 0) {
-            w /= 2;
-            res *= 2;
-          }
-          while (h % 2 == 0) {
-            h /= 2;
-            res *= 2;
-          }
-          cout << (res >= n ? "YES\n" : "NO\n");
+    short n,m;
+    cin >> n >> m;
+    char a[n][m];
+    bool is_black_white = true;
+    for(short i = 0; i<n; i++){
+        for(short j = 0; j<m; j++){
+            cin >> a[i][j];
+            if(a[i][j] != 'B' && a[i][j] != 'W' && a[i][j] != 'G'){
+                is_black_white = false;
+                break;
+            }
+        }
     }
+    if(is_black_white)
+        cout << "#Black&White";
+    else 
+        cout << "#Color";
     return 0;
 }
