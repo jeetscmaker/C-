614d60875322a7f25f931b29b25a317631d592da
Jitendra <jeetscmaker@gmail.com>
2021-06-22 10:29:35 +0530

    source of formula for Digital Root

--- C++/a.cpp
+++ C++/a.cpp
@@ -2,6 +2,7 @@
  
 using namespace std;
  // contest: April Fools Day Contest 2021, problem: (B) DMCA
+ // source of formula is: https://en.wikipedia.org/wiki/Digital_root
 int main() {
   int x;
   cin >> x;
