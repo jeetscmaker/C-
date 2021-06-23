b8b3faa58d685351d9f32e7353d3e46135af440c
Jitendra <jeetscmaker@gmail.com>
2021-06-23 11:08:51 +0530

    contest: Codeforces Round #693 (Div. 3), problem: (B) Fair Division

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,32 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// contest: Codeforces Round #686 (Div. 3), problem: (A) Special Permutation
+// contest: Codeforces Round #693 (Div. 3), problem: (B) Fair Division
 int main() {
    int t;
    cin >> t;
    while(t--){
       int n;
-      cin >> n;
-      if (n%2 == 0) 
-      {
-         for (int i = n; i >= 1; --i)
-         {
-            cout << i << " ";
-         }
-      } else {
-         for (int i = n; i >= 1; --i)
-         {
-            if (i == (n/2)+1)
-            {
-               cout << (i-1) << " " << i << " ";
-               --i;
-            }
-            else
-               cout << i << " ";
-         }
-      }
-      cout << endl;
+  cin >> n;
+  int cnt1 = 0, cnt2 = 0;
+  for (int i = 0; i < n; i++) {
+    int c;
+    cin >> c;
+    if (c == 1) {
+      cnt1++;
+    } else {
+      cnt2++;
+    }
+  }
+  if ((cnt1 + 2 * cnt2) % 2 != 0) {
+    cout << "NO\n";
+  } else {
+    int sum = (cnt1 + 2 * cnt2) / 2;
+    if (sum % 2 == 0 || (sum % 2 == 1 && cnt1 != 0)) {
+      cout << "YES\n";
+    } else {
+      cout << "NO\n";
+    }
+  }
    }
     return 0;
 }
