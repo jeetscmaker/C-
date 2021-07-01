985abeb63051994db50bcf3ddf67d73e1a157a2e
Jitendra <jeetscmaker@gmail.com>
2021-07-02 00:49:55 +0530

    contest: Educational Codeforces Round 110 (Rated for Div. 2), problem: (A) Fair Playoff

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,27 +1,18 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-// contest: Educational Codeforces Round 102 (Rated for Div. 2), problem: (A) Replacing Elements
-int main(){
-    int t;
-    cin>>t;
-    
-    while(t--){
-      short n, d;
-      bool possible = false;
-      cin >> n >> d;
-      short num[n];
-      for (int i = 0; i < n; ++i)
-      {
-        cin >> num[i];
-      }
-      sort(num, num+n);
-      if(num[n-1] <= d) possible = true;
-      else if(num[0]+num[1] <= d) possible = true;
-      if(possible) 
-        cout << "YES" << "\n";
-      else
-        cout << "NO" << "\n";
-    }
-    return 0;
-}
+// contest: Educational Codeforces Round 110 (Rated for Div. 2), problem: (A) Fair Playoff
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+		short s1,s2,s3,s4;
+		cin >> s1 >> s2 >> s3 >> s4;
+		if (min(s1,s2) > max(s3,s4) || max(s1,s2) < min(s3,s4))
+		{
+			cout << "NO" << endl;
+		} 
+		else
+			cout << "YES" << endl;
+	}
+	return 0;
+} 
