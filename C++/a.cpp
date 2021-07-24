06f4242d68698dfdfa7eabac5004b46cd1860045
Jitendra <jeetscmaker@gmail.com>
2021-07-25 00:51:32 +0530

    contest: Codeforces Round #698 (Div. 2), problem: (A) Nezzar and Colorful Balls

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,35 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//  Codeforces Beta Round #32 (Div. 2) B. Borze code.
+// contest: Codeforces Round #698 (Div. 2), problem: (A) Nezzar and Colorful Balls
 
-int main() {
-  string s;
-  cin >> s;
-  string s1 = "";
-  for (int i = 0; s[i] != '\0'; )
-  {
-    if (s[i] == '-')
-    {
-      if (i+1<s.length())
-      {
-        if (s[i+1] == '.')
-        {
-          s1 += "1";
-          i = i+2;
-        }
-        else if(s[i+1] == '-')
-        {
-          s1 += "2";
-          i = i+2;
-        }
-      }
-    }
-    else {
-      s1 += "0";
-      i++;
-    }
-  }
-  cout << s1;
-  return 0;
+int gcd(int a, int b) {
+	return b == 0 ? a : gcd(b, a%b);
+}
+
+int main(){
+	short t;
+	cin >> t;
+	while(t--){
+		short n;
+		cin >> n;
+		short a[n];
+		short current_occurrence = 1, max_occurrence = 0;
+		for(short i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		for(short i = 0; i+1 < n; i++) {
+			if(a[i] == a[i+1]) {
+				current_occurrence++;
+			} else {
+				max_occurrence = current_occurrence > max_occurrence ? current_occurrence : max_occurrence;
+				current_occurrence = 1;
+			}
+		}
+		max_occurrence = current_occurrence > max_occurrence ? current_occurrence : max_occurrence;
+		cout << max_occurrence << endl;
+	}
+	return 0;
 }
