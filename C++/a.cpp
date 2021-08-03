3e7ef7b1880aa5d43317a3aa48709cc09e1d9d88
Jitendra <jeetscmaker@gmail.com>
2021-08-03 17:35:56 +0530

    Codeforces Round #690 (Div. 3) B. Last Year's Substring

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,33 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// contest: Codeforces Round #698 (Div. 2), problem: (A) Nezzar and Colorful Balls
+// Codeforces Round #690 (Div. 3) B. Last Year's Substring
 
 int gcd(int a, int b) {
 	return b == 0 ? a : gcd(b, a%b);
 }
 
-int main(){
-	short t;
+int main() {
+	int t;
 	cin >> t;
-	while(t--){
-		short n;
+	while (t--) {
+		int n;
 		cin >> n;
-		short a[n];
-		short current_occurrence = 1, max_occurrence = 0;
-		for(short i = 0; i < n; i++) {
-			cin >> a[i];
-		}
-		for(short i = 0; i+1 < n; i++) {
-			if(a[i] == a[i+1]) {
-				current_occurrence++;
-			} else {
-				max_occurrence = current_occurrence > max_occurrence ? current_occurrence : max_occurrence;
-				current_occurrence = 1;
-			}
-		}
-		max_occurrence = current_occurrence > max_occurrence ? current_occurrence : max_occurrence;
-		cout << max_occurrence << endl;
+		string s;
+		cin >> s;
+		if(s[0] == '2' && s[n-1] == '0' && s[n-2] == '2' && s[n-3] == '0'
+			|| s[0] == '2' && s[1] == '0' && s[n-1] == '0' && s[n-2] == '2'
+			|| s[0] == '2' && s[1] == '0' && s[2] == '2' && s[n-1] == '0'
+			|| s[0] == '2' && s[1] == '0' && s[2] == '2' && s[3] == '0'
+			|| s[n-1] == '0' && s[n-2] == '2' && s[n-3] == '0' && s[n-4] == '2'
+		)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
 	}
-	return 0;
+    return 0;
 }
