9d1b2cfb5146a81c2aff4bdacb054034e9c8ae04
Jitendra <jeetscmaker@gmail.com>
2021-08-05 01:18:55 +0530

    Educational Codeforces Round 82(Div. 2), problem: (A)Erasing Zeroes

--- C++/a.cpp
+++ C++/a.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// contest: Codeforces Round #700 (Div. 2), problem: (A) Yet Another String Game
+// Educational Codeforces Round 82(Div. 2), problem: (A)Erasing Zeroes
 
 int main() {
     int t;
@@ -10,19 +10,31 @@ int main() {
     while (t--) {
         cin >> s;
         int n = s.length();
+        int count = 0;
+        int current_index_of_1 = -1;
         for (int i = 0; i < n; ++i)
         {
-            if (i%2 == 0) // Alice's turn to move.
-            {
-                if (s[i] != 'a') s[i] = 'a';
-                else s[i] = 'b';
-            }
-            else { // Bob's turn to move.
-                if (s[i] != 'z') s[i] = 'z';
-                else s[i] = 'y';   
+            if(s[i] == '1') {
+                current_index_of_1 = i; // first index of 1 found.
+                break;
             }
         }
-        cout << s << endl;
+        if(current_index_of_1 == -1) { // suppose there is no 1 in the given string.
+            cout << 0 << endl;
+            continue;
+        }
+        
+        for(int i = current_index_of_1+1; i<n; i++) {
+            if(s[i] == '1') {
+                // because for two consecutive 1, we should not increase the count.
+                // And the same applies for further indices of 1.
+                count += (i - current_index_of_1  - 1); 
+                // update the current_index_of_1 with the latest index of 1.
+                current_index_of_1 = i;
+            }
+        }
+        
+        cout << count << endl;
     }
     return 0;
 }
