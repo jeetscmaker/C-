5ccfe2ce6caf88be8c8b0dcc0e60fbbf9e62eb9e
Jitendra <jeetscmaker@gmail.com>
2021-08-11 01:41:36 +0530

    contest: Codeforces Round #494 (Div. 3), problem: (A) Polycarp's Pockets

--- C++/a.cpp
+++ C++/a.cpp
@@ -10,15 +10,24 @@ int gcd(int a, int b) {
 int main() {
     int n;
     cin >> n;
-    string t;
-    cin >> t;
-    string required = "";
-    int j = 0;
-    for(int i = 0; i<n; i = i+j) {
-        required += t[i];
-        j++;
+    int a[n];
+    for(int i = 0; i<n; i++) {
+        cin >> a[i];
     }
-    cout << required;
+    sort(a, a+n);
+    int c_occur = 1, max_occur = 1;
+    for(int i = 0; i<n-1; i++) {
+        if(a[i] == a[i+1]) {
+            c_occur++;
+        } 
+        else {
+            max_occur = max_occur < c_occur ? c_occur : max_occur;
+            c_occur = 1;
+        }
+    }
+    // suppose all elements are equal, so else part will never be executed. Hence this expression is necessary.
+    max_occur = max_occur < c_occur ? c_occur : max_occur;
+    cout << max_occur;
     return 0;
 }
 
