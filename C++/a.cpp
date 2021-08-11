a55edf27333ea87a57f445f451a15d464559a3cc
Jitendra <jeetscmaker@gmail.com>
2021-08-12 01:17:51 +0530

    contest: Codeforces Round #570 (Div. 3), problem: (A) Nearest Interesting Number

--- C++/a.cpp
+++ C++/a.cpp
@@ -3,31 +3,22 @@ using namespace std;
 
 // contest: Codeforces Round #723 (Div. 2), problem: (A) Mean Inequality
 
-int gcd(int a, int b) {
-    return b == 0 ? a : gcd(b, a%b);
+int sum(int a) {
+    int result = 0;
+    while (a > 0) {
+        result += a % 10;
+        a /= 10;
+    }
+    return result;
 }
 
 int main() {
-    int n;
-    cin >> n;
-    int a[n];
-    for(int i = 0; i<n; i++) {
-        cin >> a[i];
+    int a;
+    cin >> a;
+    while (sum(a) % 4 != 0) {
+        a++;
     }
-    sort(a, a+n);
-    int c_occur = 1, max_occur = 1;
-    for(int i = 0; i<n-1; i++) {
-        if(a[i] == a[i+1]) {
-            c_occur++;
-        } 
-        else {
-            max_occur = max_occur < c_occur ? c_occur : max_occur;
-            c_occur = 1;
-        }
-    }
-    // suppose all elements are equal, so else part will never be executed. Hence this expression is necessary.
-    max_occur = max_occur < c_occur ? c_occur : max_occur;
-    cout << max_occur;
-    return 0;
+    cout << a << endl;
 }
+
 
